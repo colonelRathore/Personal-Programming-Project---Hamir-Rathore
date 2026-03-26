bafa94314d36f6fc3a5a3b4749d8560b6b863b82
colonelRathore <hrathore@connect.kellettschool.com>
2026-03-26 10:05:24 +0800

    changes

---
+++ main.py
@@ -0,0 +1,190 @@
+import random
+from colorist import Color, Effect, green
+
+def GetNumPlayers():
+    num_players = int(input("How many people are playing RISK today?\n"))
+    return num_players
+
+def FetchPlayerNames(num_players):
+    player_names = []
+    for i in range(num_players):
+        name = input("What is your name?\n")
+        player_names.append(name)
+    return player_names
+
+def GetPlayersColors(player_names, num_players):
+    valid_colours = ["white", "black", "navy blue", "green", "red", "dark red", "purple", "orange", "yellow", "lime green", ""]
+    player_index = 0
+    player_colors = []
+    for i in range(num_players):
+        color = input((player_names[player_index]), ", what 16-bit color do you want?\n")
+        player_index += 1
+        while color not in valid_colours:
+            color = input("Please enter a valid color:\n")
+    player_colors.append(color)
+    player_index = 0
+    return player_colors, player_index
+
+def RollDice(num_players, player_names, player_index):
+    rolls = []
+    for i in range(num_players):
+        roll_go = input("Hello", (player_names[player_index]), "press r to roll your dice! ")
+        if roll_go == 'r':
+            roll = random.randint(1,6)
+        print((player_names[player_index]), "you rolled", roll)
+    rolls.append(roll)
+    player_index += 1
+    return rolls
+
+def CheckHighestRoll(rolls, player_names):
+    roll_sort = rolls.sort()
+    max_roll = roll_sort[0]
+    for roll in rolls:
+        if roll == max_roll:
+            roll_index = roll.index(max_roll)
+    print((player_names[roll_index]), "you got the highest roll!")
+    return roll_index
+
+def DisplayMap():
+    world_map = """⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿⣿⣿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀⠀
+⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀
+⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀⠀⢠⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⠙⠁
+⠀⠸⠟⠋⠀⠈⠙⣿⣿⣿⣿⣿⣿⣷⣦⡄⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣆⢘⣿⣯⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⢱⡿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡿⠦⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡗⠀⠈⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣉⣿⡿⢿⢷⣾⣾⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣷⣦⣶⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣤⡖⠛⠶⠤⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠙⣿⣿⠿⢻⣿⣿⡿⠋⢩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧⣤⣦⣤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠘⣧⠀⠈⣹⡻⠇⢀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣴⣿⣷⢲⣦⣤⡀⢀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠂⠛⣆⣤⡜⣟⠋⠙⠂⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠉⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣆⠀⠰⠄⠀⠉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠿⠿⣿⣿⣿⠇⠀⠀⢀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⢀⣼⠗⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"""
+    print(world_map)
+
+def DisplayProvinces():
+    provinces = """Alaska
+Columbia
+Northwestern Territories
+Prairies
+Hudsonia
+Upper Canada
+Northern Quebec
+Lower Canada
+Newfoundland
+Greenland
+Southeastern United States
+Midwest
+Great Plains
+Rocky Mountains
+Pacific Coast
+Atlantic Coast
+Mexico
+Central America
+Colombia
+Guyana
+Peru
+Amazonia
+Brazil
+Argentina
+Carribean
+Iceland
+Jan Martin
+Macronesia
+Iberia
+Normandy
+Occitania
+Benelux
+Switzerland
+Western Germany
+Eastern Germany
+Northern Italy
+Southern Italy
+Greater Austria
+Poland 
+Hungary
+The Balkans
+Romania
+Ruthenia
+The Baltics
+Scandinavia
+Anatolia
+The Causases
+Muscovy
+Astrabhan
+Novogrod
+Eastern Muscovy
+Urals
+Western Siberia
+Central Siberia
+Eastern Siberia
+Central Asia
+Mongolia
+Manchuria
+Xinjiang
+Shanxi
+China
+Tibet
+Korea
+Japan
+Taiwan
+Indochina
+Burma
+Eastern India
+Southern India
+Rajputana
+Pakistan
+Central India
+Himalayas
+Andaman and Nicobar 
+Maldives
+Lanka
+Phillipenes
+Malaysia
+Sumatra
+Borneo
+Celebes
+New Guinea
+Western Australia
+Northern Territory
+South Australia
+Queensland
+New South Wales
+Victoria and Tasmania
+New Zeeland
+Micronesia
+Fiji
+Maghreb
+Western Sahara
+West Africa
+Nigeria
+Equatorial Africa
+Central Africa
+South Africa
+Rhodesia
+Mozambique
+Madagascar
+East Africa
+Ethiopia
+Somalia
+Somalialand
+"""
+    print(provinces)
+    return provinces
+
+def ClaimProvince(roll_index, player_names, num_players, player_colors, provinces):
+    indx = 0
+    for i in range (num_players):
+        DisplayMap()
+        DisplayProvinces()
+        print("The game has begun!", (player_names[indx]), ", what province do you want?")
+        select = input()
+        while select not in provinces:
+            print("Please select a valid province:")
+            DisplayProvinces()
+            select = input()
+        
