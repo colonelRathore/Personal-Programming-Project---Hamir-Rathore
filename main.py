c0035fbad48aa79d71031be3105f40c2f50f7f93
colonelRathore <hrathore@connect.kellettschool.com>
2026-04-30 05:40:24 +0000

    30 Apr 2026, 13:40

--- main.py
+++ main.py
@@ -1,49 +1,6 @@
 import random
+from PreGameSetup import *
 from colorist import Color, Effect, BrightColor, BgColor, BgBrightColor
-
-def GetNumPlayers():
-    num_players = int(input("How many people are playing RISK today?\n"))
-    return num_players
-
-def FetchPlayerNames(num_players):
-    player_names = []
-    for i in range(num_players):
-        name = input("What is your name?\n")
-        player_names.append(name)
-    return player_names
-
-def GetPlayersColors(player_names, num_players):
-    valid_colours = ["white", "black", "navy blue", "green", "red", "dark red", "purple", "orange", "yellow", "lime green", ""]
-    player_index = 0
-    player_colors = []
-    for i in range(num_players):
-        color = input((player_names[player_index]), ", what 16-bit color do you want?\n")
-        player_index += 1
-        while color not in valid_colours:
-            color = input("Please enter a valid color:\n")
-    player_colors.append(color)
-    player_index = 0
-    return player_colors, player_index
-
-def RollDice(num_players, player_names, player_index):
-    rolls = []
-    for i in range(num_players):
-        roll_go = input("Hello", (player_names[player_index]), "press r to roll your dice! ")
-        if roll_go == 'r':
-            roll = random.randint(1,6)
-        print((player_names[player_index]), "you rolled", roll)
-    rolls.append(roll)
-    player_index += 1
-    return rolls
-
-def CheckHighestRoll(rolls, player_names):
-    roll_sort = rolls.sort()
-    max_roll = roll_sort[0]
-    for roll in rolls:
-        if roll == max_roll:
-            roll_index = roll.index(max_roll)
-    print((player_names[roll_index]), "you got the highest roll!")
-    return roll_index
 
 def Gen_Prov_List(num_players):
     big_prov_list = []
@@ -183,7 +140,7 @@ Somalialand
     print(provinces)
     return provinces
 
-def ClaimProvince(roll_index, player_names, num_players, player_colors, big_prov_list, provinces):
+def ClaimProvince(roll_index, player_names, num_players, big_prov_list, provinces):
     roll_index = 0
     indx = 0
     while provinces != """""":
