1e6d1ef1c1d2044a056cb97f810a0bd5a734fbdd
colonelRathore <hrathore@connect.kellettschool.com>
2026-05-20 09:21:59 +0000

    20 May 2026, 17:21

--- launch_attack.py
+++ launch_attack.py
@@ -2,6 +2,7 @@ from PreGameSetup import *
 from main import *
 from DisplayMap import *
 from playsound import *
+from TakeTurn import *
 
 def Launch_Attack():
     print("You have chosen to launch an attack! Let's hope it's successful!")
@@ -16,26 +17,30 @@ def Select_Attack_Province(provinces, playname):
 
     return attack_choice
 
-def Display_Province_Stats(attack_choice, provinces):
+def Display_Province_Stats(provinces, prov_with_commando):
     print("What province's statistics would you like to inspect?")
     choise = input()
     while choise not in provinces:
         choise = input()
+    if choise in prov_with_commando:
+        print("The province you have chosen has 2 infantry armies and one tank army.")
+        print("It has at least one commando army")
+    else:
+         print("The province you have chosen has 2 infantry armies and one tank army.")
 
+def Select_Attacker_Province(provinces):
+    print("What province would you like to attack?")
+    choise = input()
+    while choise not in provinces:
+        print("Please enter a valid option. Mr. Hall if you're doing this to test the robustness, hello from Hamir.")
 
-def Move_Troops_Attack(roll_index, player_names, num_players, player_colors, big_prov_list, provinces):
-    opt = ""
-
-def Select_Attacker_Province(roll_index, player_names, num_players, player_colors, big_prov_list, provinces):
-    opt = ""
-
-def Check_If_Province_Valid(roll_index, player_names, num_players, player_colors, big_prov_list, provinces):
-    opt = ""
 
 #RollDice()
 
 def Play_War_Music():
     playsound("leningrad.mp3")
+    playsound("lmusic1.mp3")
+    playsound("lmusic2.mp3")
 
 def Find_Roll_Sum(roll_index, player_names, num_players, player_colors, big_prov_list, provinces):
     opt = ""
